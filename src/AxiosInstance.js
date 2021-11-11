786cbaf3852b9b2df15f2cbb357febedc5927f86
suhaas-d <suhaasd13@gmail.com>
2021-11-12 01:14:27 +0530

    Axios base changed

--- src/AxiosInstance.js
+++ src/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:8000/"
+    baseURL: "https://murmuring-bayou-16041.herokuapp.com/"
 });
 
 export default AxiosInstance;
